946b320f3cf49c19a659f5f67e502b2b40c01981
Milton Soares <eng.milton.soares@gmail.com>
2020-11-19 07:42:28 -0300

    First commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function imgSlider (src) {
-  document.querySelector('.starbucks').src = src;
-}
-
-function changeColor(color){
-  const circle = document.querySelector('.circle');
-  circle.style.background = color;
-}
---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function imgSlider (src) {
+  document.querySelector('.starbucks').src = src;
+}
+
+function changeColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
